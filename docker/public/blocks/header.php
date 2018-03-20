8b062f6be63986ac6510ace9a66178f321f8f4aa
masha <byseledka@gmail.com>
2018-03-20 19:41:11 +0300

    super clear

---
+++ docker/public/blocks/header.php
@@ -0,0 +1,39 @@
+<header>
+  <div class="header-content">
+      <a href="/index.html"><img src="./img/logo.png"></a>
+      <div id="special-nav">
+          <a href="/basket.html" id="basket">Корзина(0)</a>
+          <a href="#" class="active">
+              Ru
+          </a>
+          <a href="#">
+              En
+          </a>
+      </div>
+      <nav id="main-menu">
+          <ul>
+              <li>
+                  <a href="/index.html" class="active">О нас</a>
+              </li>
+              <li>
+                  <a href="/store.html">Магазин</a>
+              </li>
+              <li>
+                  <a href="/blog.html">Блог</a>
+              </li>
+              <li>
+                  <a href="/faq.html">FAQ</a>
+              </li>
+              <li>
+                  <a href="/contacts.html">Контакты</a>
+              </li>
+              <li>
+                  <a href="/reviews.html">Отзывы</a>
+              </li>
+              <li>
+                  <a href="/partners.html">Партнеры</a>
+              </li>
+          </ul>
+      </nav>
+    </div>
+</header>
