9072528f420e708e6c83ff3b4c88e3864a302fed
masha <byseledka@gmail.com>
2018-04-19 13:47:36 +0300

    try oop menu

--- docker/public/try.php
+++ docker/public/try.php
@@ -1,18 +1,76 @@
 <?php
-$args = array();
 
-function is_active ($page, $args) {
-  foreach ($args as $value){
-    var_dump($args);
-    if ($value == $page){
-      echo 'correct';
+  class Menu {
+    var $items = [];
 
-    } else {
-      echo 'not';
-    };
+    function __construct ($items) {
+
+      foreach ($items as $value) {
+        $value = new MenuItem ($value['link_name'], $value['link']);
+        $value->showItem();
+      }
+    }
+  }
+
+
+  class MenuItem {
+    var $name;
+    var $link;
+    var $active_item;
+
+
+    function MenuItem ($page_name, $page_link) {
+      $this->name = $page_name;
+      $this->link = $page_link;
+    }
+
+
+    public function showItem () {
+      echo "<li><a href= ".$this->link."> ".$this->name." </a></li>";
+    }
+
   };
-};
-
-is_active('lala', ['all', 'noll', 'lala']);
 
 ?>
+
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>белкин дом</title>
+        <link href="./styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+
+<div class="header-content">
+    <a href="/index.html"><img src="./img/logo.png"></a>
+    <div id="special-nav">
+        <a a href="/basket.html" id="basket">Корзина(0)</a>
+        <a href="#" class="active">
+            Ru
+        </a>
+        <a href="#">
+            En
+        </a>
+    </div>
+
+    <nav id="main-menu">
+        <ul>
+          <?php
+          $menu = new Menu([
+            ["link_name" => "О нас", "link" => "/index.php"],
+            ["link_name" => "Магазин", "link" => "/store.php"],
+            ["link_name" => "Блог", "link" => "/blog.php"],
+            ["link_name" => "FAQ", "link" => "/faq.php"],
+            ["link_name" => "Контакты", "link" => "/contacts.php"],
+            ["link_name" => "Отзывы", "link" => "/reviews.php"],
+            ["link_name" => "Партнеры", "link" => "/partners.php"]
+          ]);
+           ?>
+        </ul>
+    </nav>
+  </div>
+</header>
+</body>
+</html>
